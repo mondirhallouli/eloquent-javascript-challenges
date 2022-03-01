09efd1341144bc79c048a498f63d27c95b98d0f1
mondirhallouli <halloulim@gmail.com>
2022-03-01 20:35:24 +0100

    added solution of part 2 of the list exercise, transforming a list to an array with tests included

--- chapter-4/list.js
+++ chapter-4/list.js
@@ -19,4 +19,25 @@ const arrayToList = (arr) => {
   return list;
 };
 
-module.exports = {arrayToList};
+// list to array
+const listToArray = (list) => {
+  // create our output array
+  const arr = [];
+  // check if the object is empty return an empty array
+  if(Object.keys(list).length === 0) return arr;
+  /* loop through the object and
+    1- create a binding to the list object
+    2- check if it has a valid value(an object)
+    3- push the value in the object's value property to our array
+    4- update the binding we created to point to the rest porperty of the current object, because it has our next value
+    5- start over from step 2
+
+    - once the binding inside the for loop evaluates to a falsy value(null in this case), we break out of the loop and return our updated array
+  */
+  for (let node = list; node; node = node.rest) {
+    arr.push(node.value);
+  }
+  return arr;
+};
+
+module.exports = {arrayToList, listToArray};
--- chapter-4/list.test.js
+++ chapter-4/list.test.js
@@ -1,7 +1,17 @@
 const list = require('./list');
 const arrayToList = list.arrayToList;
+const listToArray = list.listToArray;
 
+// tests for the arrayToList function
 describe('builds up a list structure', () => {
+  test('when given an empty array', () => {
+    expect(arrayToList([])).toStrictEqual({});
+  });
+
+  test('when given an array with 1 value', () => {
+    expect(arrayToList([3])).toStrictEqual({ value: 3, rest: null });
+  });
+
   test('when given [1, 2, 3]', () => {
     expect(arrayToList([1,2,3])).toStrictEqual({
       value: 1,
@@ -14,12 +24,28 @@ describe('builds up a list structure', () => {
       }
     });
   });
+});
 
-  test('empty array', () => {
-    expect(arrayToList([])).toEqual({});
+// tests for the listToArray function
+describe('produces an array from a list', () => {
+  test('when given an empty list', () => {
+    expect(listToArray({})).toStrictEqual([]);
   });
 
-  test('array with 1 value', () => {
-    expect(arrayToList([3])).toEqual({ value: 3, rest: null });
+  test('when given a list with 1 value', () => {
+    expect(listToArray({value: 1, rest: null})).toStrictEqual([1]);
+  });
+
+  test('when given a list', () => {
+    expect(listToArray({
+      value: 1,
+      rest: {
+        value: 2,
+        rest: {
+          value: 3,
+          rest: null
+        }
+      }
+    })).toStrictEqual([1,2,3]);
   });
 });
