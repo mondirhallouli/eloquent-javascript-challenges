d52ffe97bdcf396cbbfca575ebb6941d05de1403
mondirhallouli <halloulim@gmail.com>
2024-01-14 16:14:56 +0100

    added comment, mousetrail.js, chapter 15

--- chapter-15/mousetrail.js
+++ chapter-15/mousetrail.js
@@ -1,5 +1,5 @@
 
-for (let i = 0; i < 60; i++) {
+for (let i = 0; i < 6; i++) {
     let div = document.createElement('div');
     div.classList.add("trail");
     document.body.appendChild(div);
@@ -11,5 +11,6 @@ document.addEventListener('mousemove', (e) => {
     let currDiv = divs[i];
     currDiv.style.left = (e.pageX - 4) + 'px';
     currDiv.style.top = (e.pageY - 4) + 'px';
+    // this gets us the next div in the divs array
     i = (i + 1) % divs.length;
 })
