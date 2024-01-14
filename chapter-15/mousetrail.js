4964b7040756183ec3e28217ec4b806670dd313a
mondirhallouli <halloulim@gmail.com>
2024-01-14 16:09:03 +0100

    chapter 15 mousetrail exercise solution

---
+++ chapter-15/mousetrail.js
@@ -0,0 +1,15 @@
+
+for (let i = 0; i < 60; i++) {
+    let div = document.createElement('div');
+    div.classList.add("trail");
+    document.body.appendChild(div);
+}
+
+let divs = Array.from(document.querySelectorAll('.trail'));
+let i = 0;
+document.addEventListener('mousemove', (e) => {
+    let currDiv = divs[i];
+    currDiv.style.left = (e.pageX - 4) + 'px';
+    currDiv.style.top = (e.pageY - 4) + 'px';
+    i = (i + 1) % divs.length;
+})
